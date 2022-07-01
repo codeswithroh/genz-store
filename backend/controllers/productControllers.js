e0164b80d48d0bfeac9582ffe7064d078a1f8d28
codeswithroh <codeswithroh@gmail.com>
2022-07-02 01:41:32 +0530

    added CRUD to the api

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -25,8 +25,36 @@ exports.updateProduct = async (req, res, next) => {
   res.status(200).json({ success: true, product });
 };
 
+// delete products -- admin
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  await product.remove();
+  res
+    .status(200)
+    .json({ success: true, message: "Product deleted successfully" });
+};
+
 // get all products
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
   res.status(200).json({ success: true, products });
+};
+
+// get product by id
+exports.getProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+  res.status(200).json({ success: true, product });
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,8 @@ const {
   getAllProducts,
   createProduct,
   updateProduct,
+  deleteProduct,
+  getProduct,
 } = require("../controllers/productControllers");
 
 const router = express.Router();
@@ -10,6 +12,10 @@ const router = express.Router();
 // routes
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
-router.route("/product/:id").put(updateProduct);
+router
+  .route("/product/:id")
+  .put(updateProduct)
+  .delete(deleteProduct)
+  .get(getProduct);
 
 module.exports = router;
