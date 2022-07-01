9d3a2e3173d394ad9ea002406b9a5d446bbba1b7
codeswithroh <codeswithroh@gmail.com>
2022-07-01 20:49:43 +0530

    added CRU features in api

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,3 +1,32 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+const Product = require("../models/productModels");
+
+// create products -- admin
+exports.createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(200).json({ success: true, product });
+};
+
+// update products -- admin
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({ success: true, product });
+};
+
+// get all products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({ success: true, products });
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productControllers");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+} = require("../controllers/productControllers");
 
 const router = express.Router();
 
 // routes
 router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
